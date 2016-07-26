88f180a2177ec40ce3f1f35712ce023d79a9b706
Tempurturtul <tempurturtul@gmail.com>
2016-07-26 19:47:57 -0400

    Confirm concerned comments and comment-out offending code

--- src/scripts/utils/past.test.js
+++ src/scripts/utils/past.test.js
@@ -2,16 +2,19 @@ var test = require('tape');
 var path = require('path');
 
 // Execute past.js.
-// (Does this produce side effects in other test files?)
-// (Isolate execution within individual tests?)
-require(path.join(process.cwd(), 'src/scripts/utils/past.js'));
+// (Does this produce side effects in other test files?) YES!
+// require(path.join(process.cwd(), 'src/scripts/utils/past.js'));
 
 test('past is defined on Date', function(t) {
+  // Execute past.js.
+  // (Does this produce side effects in other test files?) YES!
+  // require(path.join(process.cwd(), 'src/scripts/utils/past.js'));
+
   t.ok(Date.past, 'past is defined');
   t.end();
 });
 
 // Expecting failure.
 test.skip('past handles multiple-digit numbers', function(t) {
-  
+
 });
