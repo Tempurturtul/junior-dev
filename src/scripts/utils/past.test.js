6da6e2c7933e0c574e888380b8136ef6f17ebd19
Matt Feidt <tempurturtul@gmail.com>
2016-07-26 16:54:57 -0400

    Add concerned comments

--- src/scripts/utils/past.test.js
+++ src/scripts/utils/past.test.js
@@ -2,6 +2,8 @@ var test = require('tape');
 var path = require('path');
 
 // Execute past.js.
+// (Does this produce side effects in other test files?)
+// (Isolate execution within individual tests?)
 require(path.join(process.cwd(), 'src/scripts/utils/past.js'));
 
 test('past is defined on Date', function(t) {
